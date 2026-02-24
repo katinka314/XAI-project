e852545483f933c72f5497b12860390d8f7faaf3
avkol22 <s234136@dtu.dk>
2026-02-24 11:54:55 +0100

    final additions

--- dataprocessing.py
+++ dataprocessing.py
@@ -6,11 +6,12 @@ import pandas as pd
 
 data = pd.read_csv('data/adult.data')
 
-pred = data.iloc[:,-1]
-print(pred)
 
 def data_processing(data):
     data = np.array(data)
     x = data[:,0]
-    y = data[:,1]
+    y = data[:,-1]
+    
     return x,y
+
+x,y = data_processing(data)
