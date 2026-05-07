b54944e0798a0d2d689effe270f9ec3f374464c8
avkol22 <s234136@dtu.dk>
2026-05-07 13:09:21 +0200

    adding nn to sex confusion plot

--- stat.py
+++ stat.py
@@ -10,14 +10,16 @@ from src.data_transformation import (
     load_adult_data,
     split_X_y,
 )
-from src.model_factory import fit_and_score, model_dt, model_lr
+from src.model_factory import fit_and_score, model_dt, model_MLP
 
 data = load_adult_data(data_path='data/adult.data', nrows=None)
 X, y = split_X_y(data)
 X_train, X_test, y_train, y_test = create_train_test_split(X, y, test_size=0.2, random_state=42)
 dt_model = model_dt(categorical_cols, numeric_cols)
+nn_model = model_MLP(categorical_cols, numeric_cols)
 
 dt_score = fit_and_score(dt_model, X_train, y_train, X_test, y_test)
+nn_score = fit_and_score(nn_model, X_train, y_train, X_test, y_test)
 
 #%%
 # Fix X_test once, at the top
@@ -32,11 +34,16 @@ X_female, y_female = X_test[female_mask].copy(), y_test[female_mask]
 
 
 
-# Confusion Matrix
+# Confusion Matrix - Decision Tree
 dtf_confusion = plot_confusion_matrix(dt_model, X_female, y_female, "Female Confusion DT", dir_path='src/DataScientist/', plot=True, vmax=None)
 dtm_confusion = plot_confusion_matrix(dt_model, X_male, y_male, "Male Confusion DT", dir_path='src/DataScientist/', plot=True, vmax=None)
 
-print(dtm_confusion)
+#Confusion Matrix - Neural Network
+nnf_confusion = plot_confusion_matrix(nn_model, X_female, y_female, "Female Confusion NN", dir_path='src/DataScientist/', plot=True, vmax=None)
+nnm_confusion = plot_confusion_matrix(nn_model, X_male, y_male, "Male Confusion NN", dir_path='src/DataScientist/', plot=True, vmax=None)
+
+
+
 #%%
 
 # Fairness Metrics - Equalized Odds
@@ -45,9 +52,16 @@ dtf_tpr = dtf_confusion["TP"] / (dtf_confusion["FN"] + dtf_confusion["TP"])  # T
 dtm_fpr = dtm_confusion["FP"] / (dtm_confusion["TN"] + dtm_confusion["FP"])  # False Positive Rate for Males
 dtm_tpr = dtm_confusion["TP"] / (dtm_confusion["FN"] + dtm_confusion["TP"])  # True Positive Rate for Males
 
+nnf_fpr = nnf_confusion["FP"] / (nnf_confusion["TN"] + nnf_confusion["FP"])  # False Positive Rate
+nnf_tpr = nnf_confusion["TP"] / (nnf_confusion["FN"] + nnf_confusion["TP"])  # True Positive Rate
+nnm_fpr = nnm_confusion["FP"] / (nnm_confusion["TN"] + nnm_confusion["FP"])  # False Positive Rate
+nnm_tpr = nnm_confusion["TP"] / (nnm_confusion["FN"] + nnm_confusion["TP"])  # True Positive Rate
+
 print(f"Female FPR: {dtf_fpr:.4f},  Female TPR: {dtf_tpr:.4f}")
 print(f"Male FPR: {dtm_fpr:.4f},  Male TPR: {dtm_tpr:.4f}")
 
+print(f"Female FPR: {nnf_fpr:.4f},  Female TPR: {nnf_tpr:.4f}")
+print(f"Male FPR: {nnm_fpr:.4f},  Male TPR: {nnm_tpr:.4f}")
 
 
 
